95f3ee6f89447b313a0f708b469b786ba1bd78ac
Peer Rails <peer@omck.ws>
2015-05-29 17:00:28 +0300

    change to 8080 port

--- puma.rb
+++ puma.rb
@@ -2,7 +2,7 @@ environment ENV["RAILS_ENV"] || 'development'
 pidfile 'pids/puma.pid'
 state_path 'pids/puma.state'
 stdout_redirect nil, 'log/stderr.log', true
-bind "tcp://0.0.0.0:#{ENV["PUMAPORT"] || 3000}"
+bind "tcp://0.0.0.0:#{ENV["PUMAPORT"] || 8080}"
 quiet
 tag 'tweet2vk'
 workers ENV["RAILS_WORKERS"] || 1
